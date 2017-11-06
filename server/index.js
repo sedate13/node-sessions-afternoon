c5fdca1fb33d482ac6099f93ead68695b8793997
valentina abramova <valentina@v.local>
2017-11-05 18:50:07 -0600

    done

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const checkForSession= require ('./middlewares/checkForSession');
 const app = express();
 const swag_controller= require ('./controllers/swag_controller');
 const auth_controller = require('./controllers/auth_controller');
+const cart_controller= require ('./controllers/cart_controller');
+const search_controller= require('./controllers/search_controller');
+
 
 app.use(bodyParser.json());
 app.use(session({
@@ -16,12 +19,21 @@ app.use(session({
 
 app.use(checkForSession);
 
+
+app.use( express.static( `${__dirname}/../public/build` ) );
+
 app.get ('/api/swag', swag_controller.read);
 
 app.post('/api/login', auth_controller.login);
 app.post( '/api/register', auth_controller.register );
 app.post( '/api/signout', auth_controller.signout );
 app.get( '/api/user', auth_controller.getUser );
+
+app.post ('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete ('/api/cart', cart_controller.delete);
+
+app.get ('/api/search', search_controller.search)
 
 
 
